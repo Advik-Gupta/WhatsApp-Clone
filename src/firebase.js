2fa3509804b6c9ad545282ea1105e6d130518c49
Advik Gupta <83698727+Advik-Gupta@users.noreply.github.com>
2022-01-27 14:56:53 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 const firebaseConfig = {
-	apiKey: 'AIzaSyCj3iRrlS59rwbld-a5E-GqM_MWHHAIDGE',
-	authDomain: 'whatsapp-clone-bee29.firebaseapp.com',
-	projectId: 'whatsapp-clone-bee29',
-	storageBucket: 'whatsapp-clone-bee29.appspot.com',
-	messagingSenderId: '550855906246',
-	appId: '1:550855906246:web:fa3024e515d75fb39998c7',
-	measurementId: 'G-22R5DRL3G8'
+	apiKey: '',
+	authDomain: '',
+	projectId: '',
+	storageBucket: '',
+	messagingSenderId: '',
+	appId: '',
+	measurementId: ''
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
